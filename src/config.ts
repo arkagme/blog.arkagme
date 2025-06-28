8afed621110f2ef8628d3b8992d6c0c9bae1945c
Arkaprava Gaine <codearka2002@gmail.com>
2025-06-28 12:07:12 +0530

    favicon

--- src/config.ts
+++ src/config.ts
@@ -37,31 +37,31 @@ export const siteConfig: SiteConfig = {
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		// }
 		{
-			src: '/favicon/favicon-16x16.png',
-			sizes: '16x16',
+			src: "/favicon/favicon-16x16.png",
+			sizes: "16x16",
 		},
 		{
-			src: '/favicon/favicon-32x32.png',
-			sizes: '32x32',
+			src: "/favicon/favicon-32x32.png",
+			sizes: "32x32",
 		},
 		{
-			src: '/favicon/favicon-48x48.png',
-			sizes: '48x48',
+			src: "/favicon/favicon-48x48.png",
+			sizes: "48x48",
 		},
 		{
-			src: '/favicon/favicon-96x96.png',
-			sizes: '96x96',
+			src: "/favicon/favicon-96x96.png",
+			sizes: "96x96",
 		},
 		{
-			src: '/favicon/favicon-192x192.png',
-			sizes: '192x192',
+			src: "/favicon/favicon-192x192.png",
+			sizes: "192x192",
 		},
 		{
-			src: '/favicon/apple-touch-icon.png',
-			sizes: '180x180',
+			src: "/favicon/apple-touch-icon.png",
+			sizes: "180x180",
 		},
 		{
-			src: '/favicon/favicon.ico',
+			src: "/favicon/favicon.ico",
 		},
 	],
 };
