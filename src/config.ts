d1348da702d1af7259e20c56dbcd3e0567343606
Arkaprava Gaine <codearka2002@gmail.com>
2025-06-28 11:59:01 +0530

    favicon

--- src/config.ts
+++ src/config.ts
@@ -36,6 +36,33 @@ export const siteConfig: SiteConfig = {
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		// }
+		{
+			src: '/favicon/favicon-16x16.png',
+			sizes: '16x16',
+		},
+		{
+			src: '/favicon/favicon-32x32.png',
+			sizes: '32x32',
+		},
+		{
+			src: '/favicon/favicon-48x48.png',
+			sizes: '48x48',
+		},
+		{
+			src: '/favicon/favicon-96x96.png',
+			sizes: '96x96',
+		},
+		{
+			src: '/favicon/favicon-192x192.png',
+			sizes: '192x192',
+		},
+		{
+			src: '/favicon/apple-touch-icon.png',
+			sizes: '180x180',
+		},
+		{
+			src: '/favicon/favicon.ico',
+		},
 	],
 };
 
